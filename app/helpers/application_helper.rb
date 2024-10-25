572f5b8afa4033fe288aae39ef953e3bfe2f1eb0
Mikkel Malmberg <mikkel@brnbw.com>
2024-10-25 11:44:30 +0200

    Progress, kinda

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,17 @@
 module ApplicationHelper
   def component_page(&block)
-    content_tag(:div, class: "py-12 px-5") do
+    tag.div(class: "py-12 px-5") do
       capture(&block)
     end
   end
 
   def title(text)
-    content_tag(:h1, text, class: "text-3xl font-semibold mb-6")
+    tag.h1(class: "text-3xl font-semibold mb-6") { text }
+  end
+
+  def example
+    tag.div(class: "flex justify-center items-center py-20 px-5 w-full rounded border") do
+      yield
+    end
   end
 end
