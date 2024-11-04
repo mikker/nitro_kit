8794b3e884c5cec979dfa46675953acee34fab2a
Mikkel Malmberg <mikkel@brnbw.com>
2024-11-04 13:16:20 +0100

    Enable public file server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
 
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = {"cache-control" => "public, max-age=#{1.year.to_i}"}
+  # config.public_file_server.enabled = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -86,6 +87,4 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  config.public_file_server.enabled = false
 end
