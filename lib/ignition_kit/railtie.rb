2f0adb9af0f2e6a0e3515460065ddf41eec8cea2
Mikkel Malmberg <mikkel@brnbw.com>
2024-11-06 17:16:30 +0100

    ..

--- lib/ignition_kit/railtie.rb
+++ lib/ignition_kit/railtie.rb
@@ -1,6 +1,6 @@
 module IgnitionKit
   class Railtie < ::Rails::Railtie
-    initializer("ignition_kit.initialize") do |application|
+    initializer("ignition_kit.initialize") do
       # Ignore our generators as they confuse Zeitwerk
       Rails.autoloaders.main.ignore(File.expand_path("../generators", __dir__))
     end
