b211bba055c91b9cc5bb262629caa07128743e07
Mikkel Malmberg <mikkel@brnbw.com>
2024-11-07 14:38:15 +0100

    Better 404

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,18 @@ class PagesController < ApplicationController
   end
 
   def show
-    render(params[:page])
-  rescue ActionView::MissingTemplate
-    render(file: "public/404.html", status: 404, layout: false)
+    if available_pages.include?(params[:page])
+      render(params[:page])
+    else
+      render(file: "public/404.html", status: 404, layout: false)
+    end
+  end
+
+  private
+
+  def available_pages
+    @available_pages ||= Dir[Rails.root.join("app/views/pages/*.html.erb")].map do |file|
+      file.split("/").last.split(".").first
+    end
   end
 end
