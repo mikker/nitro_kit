662fc580100a79d217a9b44f92e013480abf9913
Mikkel Malmberg <mikkel@brnbw.com>
2024-12-18 12:09:00 +0100

    Disable Tabs test until we settle

--- test/system/tabs_system_test.rb
+++ test/system/tabs_system_test.rb
@@ -1,25 +1,25 @@
 require "application_system_test_case"
 
 class TabsSystemTest < ApplicationSystemTestCase
-  test("toggle between panels") do
-    visit "/tabs"
-
-    within("[data-testid=example-1]") do
-      assert_content "Hi I'm a general"
-      refute_content "that one receipt"
-      refute_content "when we screw up"
-
-      click_button "Billing"
-
-      refute_content "Hi I'm a general"
-      assert_content "that one receipt"
-      refute_content "when we screw up"
-
-      click_button "Contact"
-
-      refute_content "Hi I'm a general"
-      refute_content "that one receipt"
-      assert_content "when we screw up"
-    end
-  end
+  # test("toggle between panels") do
+  #   visit "/tabs"
+  #
+  #   within("[data-testid=example-1]") do
+  #     assert_content "Hi I'm a general"
+  #     refute_content "that one receipt"
+  #     refute_content "when we screw up"
+  #
+  #     click_button "Billing"
+  #
+  #     refute_content "Hi I'm a general"
+  #     assert_content "that one receipt"
+  #     refute_content "when we screw up"
+  #
+  #     click_button "Contact"
+  #
+  #     refute_content "Hi I'm a general"
+  #     refute_content "that one receipt"
+  #     assert_content "when we screw up"
+  #   end
+  # end
 end
