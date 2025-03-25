be03aa9aad37547ca6d07bf7a37b5145915b3564
Mikkel Malmberg <mikkel@brnbw.com>
2025-03-25 11:34:23 +0100

    Format Component

--- app/components/nitro_kit/component.rb
+++ app/components/nitro_kit/component.rb
@@ -37,18 +37,19 @@ module NitroKit
 
     # Merge attributes with some special cases for matching keys
     def merge_attrs(*hashes, **defaults)
-      defaults.merge(*hashes) do |key, old_value, new_value|
-        case key
-        when :class
-          # Use TailwindMerge to merge class names
-          merge_class(old_value, new_value)
-        when :data
-          # Merge data hashes with some special cases for Stimulus
-          merge_data(old_value, new_value)
-        else
-          new_value
+      defaults
+        .merge(*hashes) do |key, old_value, new_value|
+          case key
+          when :class
+            # Use TailwindMerge to merge class names
+            merge_class(old_value, new_value)
+          when :data
+            # Merge data hashes with some special cases for Stimulus
+            merge_data(old_value, new_value)
+          else
+            new_value
+          end
         end
-      end
     end
 
     alias :mattr :merge_attrs
@@ -59,17 +60,19 @@ module NitroKit
     end
 
     def merge_data(*hashes)
-      hashes.compact.reduce({}) do |acc, hash|
-        acc.deep_merge(hash) do |key, old_val, new_val|
-          # Concat Stimulus actions
-          case key
-          when :action, :controller
-            [new_val, old_val].compact.join(" ")
-          else
-            new_val
+      hashes
+        .compact
+        .reduce({}) do |acc, hash|
+          acc.deep_merge(hash) do |key, old_val, new_val|
+            # Concat Stimulus actions
+            case key
+            when :action, :controller
+              [new_val, old_val].compact.join(" ")
+            else
+              new_val
+            end
           end
         end
-      end
     end
 
     def text_or_block(text = nil, &block)
