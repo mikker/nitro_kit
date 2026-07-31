57f3651eb47fd05993397e19aca79bb7e88fd7de
Mikkel Malmberg <mikkel@brnbw.com>
2026-07-31 11:34:16 +0200

    Run browser CI with desktop capabilities

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -7,7 +7,7 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include BrowserHelpers
 
   driven_by :selenium, using: :chrome, screen_size: [ 1440, 1200 ] do |options|
-    options.add_argument("--headless=new")
+    options.add_argument("--headless=new") unless ENV["HEADLESS"] == "false"
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-background-networking")
@@ -19,15 +19,6 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
     # Headless Chrome otherwise reports document.hasFocus() == false for a
     # backgrounded target and silently drops focus/focusin events.
     browser.execute_cdp("Emulation.setFocusEmulationEnabled", enabled: true)
-    browser.execute_cdp(
-      "Emulation.setEmulatedMedia",
-      features: [
-        { name: "hover", value: "hover" },
-        { name: "any-hover", value: "hover" },
-        { name: "pointer", value: "fine" },
-        { name: "any-pointer", value: "fine" }
-      ]
-    )
     resize_viewport(width: 1440, height: 1200)
   end
 
